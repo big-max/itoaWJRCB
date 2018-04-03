e457e0305bc392719bc821e563fc82b84406a295
guyuehen <hujinhz@163.com>
2018-04-03 18:48:10 +0800

    1.修改config.js 的 ftp_server为IP地址

--- WebContent/js/config.js
+++ WebContent/js/config.js
@@ -5,7 +5,7 @@ function playbook_property(){
 	 this.ftp_password="itoa"; 
 	 this.tsm_path="/tsm_path";
 	 this.downloadpath="/tmp/tsmclient";
-	 this.ftp_server="/ftp_server";
+	 this.ftp_server="192.168.80.154";
 	 this.softpath="/home/data/software/";			//软件介质路径
 	 this.serverip="192.168.80.154",
 	 this.serverport="8000",
